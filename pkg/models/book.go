e0c2a79113fdc1f3ead6bd1d3cabc5d5e85cebb2
utuedey <utuedeyejosph@gmail.com>
2024-12-15 23:06:50 +0100

    Add book model

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -8,7 +8,7 @@ import (
 var db *gorm.DB
 
 type Book struct {
-	gorm.model
+	gorm.Model
 	Name        string `gorm:""json:"name"`
 	Author      string `json:author`
 	Publication string `json:"publication"`
@@ -18,4 +18,32 @@ func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+// CreateBook func
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+// Get All Books func
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+// Get Book by Id func
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+// Delet Book by Id
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
 }
